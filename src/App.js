ebcbe08fad15bebdf5d3813587c6f4ba813866e7
Jenny Zilverberg <jennyrud@gmail.com>
2022-11-29 13:48:28 +0200

    adding select react dropdown

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select'
+
 
 function App() {
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Select options={options} />
         <a
           className="App-link"
           href="https://reactjs.org"
